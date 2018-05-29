9021d399ba75ad4d9bf7d8c90f02861466cf9c26
Annika <liebste.anniu@gmaile.com>
2018-05-29 21:07:24 +1200

    adds SHOW_ERROR

--- client/reducers/questions.js
+++ client/reducers/questions.js
@@ -2,6 +2,8 @@ const questions = (state = [], action) => {
     switch(action.type) {
         case 'RECEIVE_QUESTIONS':
             return action.questionsarray
+        case 'SHOW_ERROR':
+            return "Oh no an error"
         default:
             return state
     }
