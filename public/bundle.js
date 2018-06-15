853aa243adb85c8a03b5a765fbc1b64893fc123c
Annika <liebste.anni@gmail.com>
2018-06-15 13:48:32 +1200

    adds scoreboard responsiveness for mobile view

--- public/bundle.js
+++ public/bundle.js
@@ -24070,6 +24070,8 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.Scoreboard = undefined;
 
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
 var _react = __webpack_require__(1);
 
 var _react2 = _interopRequireDefault(_react);
@@ -24080,44 +24082,91 @@ var _score = __webpack_require__(9);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-var Scoreboard = exports.Scoreboard = function Scoreboard(props) {
-  function componentDidMount() {
-    props.dispatch((0, _score.getScoresApi)());
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var Scoreboard = exports.Scoreboard = function (_React$Component) {
+  _inherits(Scoreboard, _React$Component);
+
+  function Scoreboard() {
+    _classCallCheck(this, Scoreboard);
+
+    return _possibleConstructorReturn(this, (Scoreboard.__proto__ || Object.getPrototypeOf(Scoreboard)).apply(this, arguments));
   }
 
-  return _react2.default.createElement(
-    'div',
-    null,
-    _react2.default.createElement(
-      'div',
-      { className: 'scoreboard' },
-      _react2.default.createElement(
-        'h4',
-        null,
-        'Top Scores'
-      ),
-      _react2.default.createElement(
+  _createClass(Scoreboard, [{
+    key: 'componentDidMount',
+    value: function componentDidMount() {
+      this.props.dispatch((0, _score.getScoresApi)());
+    }
+  }, {
+    key: 'render',
+    value: function render() {
+      var firstFiveTopScores = this.props.score.topScores.slice(0, 5).map(function (score) {
+        return _react2.default.createElement(
+          'div',
+          { className: 'topscoresnamesleft' },
+          _react2.default.createElement(
+            'p',
+            null,
+            ' ',
+            score.score,
+            ' - ',
+            score.name,
+            ' '
+          )
+        );
+      });
+      var secondFiveTopScores = this.props.score.topScores.slice(5).map(function (score) {
+        return _react2.default.createElement(
+          'div',
+          { className: 'topscoresnamesright' },
+          _react2.default.createElement(
+            'p',
+            null,
+            ' ',
+            score.score,
+            ' - ',
+            score.name,
+            ' '
+          )
+        );
+      });
+      return _react2.default.createElement(
         'div',
-        { className: 'topscores' },
-        props.score.topScores.map(function (score) {
-          return [_react2.default.createElement(
+        null,
+        _react2.default.createElement(
+          'div',
+          { className: 'scoreboard' },
+          _react2.default.createElement(
+            'h4',
+            null,
+            'Top Scores'
+          ),
+          _react2.default.createElement(
             'div',
-            { className: 'topscoresnames' },
+            { className: 'topscores' },
             _react2.default.createElement(
-              'p',
-              null,
-              ' ',
-              score.score,
-              ' - ',
-              score.name,
-              ' '
+              'div',
+              { className: 'topscoresleft' },
+              firstFiveTopScores
+            ),
+            _react2.default.createElement(
+              'div',
+              { className: 'topscoresright' },
+              secondFiveTopScores
             )
-          )];
-        })
-      )
-    )
-  );
-};
+          )
+        )
+      );
+    }
+  }]);
+
+  return Scoreboard;
+}(_react2.default.Component);
 
 function mapStateToProps(state) {
   return {
